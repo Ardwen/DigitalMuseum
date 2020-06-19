38f8dd67f72c222c58c80786accd549dbb45f74e
apple <ardwen137@gmail.com>
2020-06-19 00:04:15 -0500

    ArtImage uploading service

--- src/main/java/com/example/digitalmuseum/model/Museume.java
+++ src/main/java/com/example/digitalmuseum/model/Museume.java
@@ -1,5 +1,28 @@
 package com.example.digitalmuseum.model;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+@JsonIgnoreProperties({ "handler","hibernateLazyInitializer"})
 public class Museume {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    int id;
+
+    @ManyToOne
+    @JoinColumn(name="cid")
+    private Category category;
+
+    private String name;
+    private String introduction;
+
+
+
+
 
 }
